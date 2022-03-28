d93ec297bfb43d84675d1610688e96715b09b138
Ivan <lorf1991@gmail.com>
2022-03-28 23:27:26 +0300

    views

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 
 const app = express();
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200);
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  // res.status(200);
+  // res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.status(200);
-  res.sendFile(path.join(__dirname, "views", "about.html"));
+  // res.status(200);
+  // res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.render("about");
 });
 
 app.listen(PORT, () => {
