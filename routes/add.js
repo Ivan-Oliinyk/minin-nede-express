fddf5be43a9ce26072ff2a9c9aa9294c7bc6f4d8
Ivan <lorf1991@gmail.com>
2022-03-31 14:42:33 +0300

    les 26 create models

--- routes/add.js
+++ routes/add.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
 const router = Router();
+const Course = require("../models/course");
 
 router.get("/", (req, res) => {
   res.render("add", { title: "Add", isAdd: true });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/", async (req, res) => {
+  const course = new Course(req.body.title, req.body.img, req.body.price);
 
-  res.redirect("/");
+  await course.save();
+
+  res.redirect("/courses");
 });
 
 module.exports = router;
