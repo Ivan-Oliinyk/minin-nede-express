14de4f63558d3c713e2b2349bfe87201e7967ac4
Ivan <lorf1991@gmail.com>
2022-03-31 11:44:48 +0300

    les 25 form processiong

--- routes/add.js
+++ routes/add.js
@@ -5,4 +5,10 @@ router.get("/", (req, res) => {
   res.render("add", { title: "Add", isAdd: true });
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
 module.exports = router;
