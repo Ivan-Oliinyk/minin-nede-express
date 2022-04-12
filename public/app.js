c30a890201e97694e4454a6a3052bdac1f2ae68b
Ivan <lorf1991@gmail.com>
2022-04-12 10:46:38 +0300

    les 34

--- public/app.js
+++ public/app.js
@@ -1,1 +1,22 @@
-console.log("hellow");
+document.querySelectorAll(".price").forEach((node) => {
+  node.textContent = new Intl.NumberFormat("de-DE", {
+    currency: "UAH",
+    style: "currency",
+  }).format(node.textContent);
+});
+
+const $card = document.querySelector("#card");
+
+if ($card) {
+  $card.addEventListener("click", (e) => {
+    if (e.target.classList.contains("js-remove")) {
+      const id = e.target.dataset.id;
+
+      fetch("/card/remove/" + id, {
+        method: "delete",
+      })
+        .then((res) => res.json())
+        .then((card) => console.log(card));
+    }
+  });
+}
