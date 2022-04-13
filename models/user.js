ef87880c5243094a8a0dcfa13fb484698b60f83e
Ivan <lorf1991@gmail.com>
2022-04-13 11:24:46 +0300

    1st step working with card

--- models/user.js
+++ models/user.js
@@ -36,4 +36,24 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCard = function (course) {
+  const items = [...this.card.items];
+  const idx = items.findIndex((item) => {
+    return item.courseId.toString() === course._id.toString();
+  });
+
+  if (idx >= 0) {
+    items[idx].count = items[idx].count + 1;
+  } else {
+    items.push({
+      courseId: course._id,
+      count: 1,
+    });
+  }
+
+  this.card = { items };
+
+  return this.save();
+};
+
 module.exports = model("User", userSchema);
--- routes/card.js
+++ routes/card.js
@@ -1,27 +1,34 @@
-const {Router} = require('express')
-const Card = require('../models/card')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
-router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
-  await Card.add(course)
-  res.redirect('/card')
-})
+router.post("/add", async (req, res) => {
+  try {
+    const course = await Course.findById(req.body.id);
+    console.log("req.user", req.user);
 
-router.delete('/remove/:id', async (req, res) => {
-  const card = await Card.remove(req.params.id)
-  res.status(200).json(card)
-})
+    await req.user.addToCard(course);
+    res.redirect("/card");
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-router.get('/', async (req, res) => {
-  const card = await Card.fetch()
-  res.render('card', {
-    title: 'Корзина',
-    isCard: true,
-    courses: card.courses,
-    price: card.price
-  })
-})
+router.delete("/remove/:id", async (req, res) => {
+  const card = await Card.remove(req.params.id);
+  res.status(200).json(card);
+});
 
-module.exports = router
+router.get("/", async (req, res) => {
+  // const card = await Card.fetch();
+  // res.render("card", {
+  //   title: "Корзина",
+  //   isCard: true,
+  //   courses: card.courses,
+  //   price: card.price,
+  // });
+
+  res.json({ test: true });
+});
+
+module.exports = router;
